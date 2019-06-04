f9c4fe2d89b50b8971e8ce462ea8d77d411a6bbd
Tony Aldridge <tony@dabapps.com>
2019-06-04 22:43:36 +0100

    Start storing times

--- src/main/scala/timetracker/Timetracker.scala
+++ src/main/scala/timetracker/Timetracker.scala
@@ -1,11 +1,15 @@
 package timetracker
+import scala.collection.SortedMap
 import scala.util.control.Breaks._
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
 import com.googlecode.lanterna.screen.TerminalScreen
 import com.googlecode.lanterna.input.{KeyStroke, KeyType}
 
+class TimeStart(val name: String, val start: Long)
 
 object Timetracker extends App {
+  def now() = System.currentTimeMillis
+
   val terminal = new DefaultTerminalFactory().createTerminal()
   val screen = new TerminalScreen(terminal)
   screen.startScreen()
@@ -14,6 +18,10 @@ object Timetracker extends App {
   val textGraphics = screen.newTextGraphics()
   screen.setCursorPosition(null)
   screen.refresh()
+
+  val start = now()
+
+  var currentTimes = List(new TimeStart("Nothing", now()), new TimeStart("Something", now() + 10))
 
   breakable {
     while (true) {
@@ -30,6 +38,9 @@ object Timetracker extends App {
       }
       // Draw
       textGraphics.putString(0, terminalSize.getRows - 1, "Logging time for blahblah")
+      currentTimes.zipWithIndex.foreach { case(time, idx) => {
+        textGraphics.putString(0, idx, s"${time.name} - ${time.start}")
+      }}
       screen.refresh()
       Thread.`yield`()
     }
